710848d436203c67434e11e49835ef0d1354c769
Dirk Munro <dirkmunro8@gmail.com>
2023-10-31 17:45:58 +0100

    par

--- kain.py
+++ kain.py
@@ -2,8 +2,8 @@
 import os
 import sys
 import vtk
+import pickle
 import numpy as np
-import multiprocessing as mp
 from scipy.optimize import differential_evolution
 #
 def conv(xk,convergence):
@@ -39,48 +39,61 @@ def wout(vtp,fln,k):
 #
     writer = vtk.vtkXMLPolyDataWriter()
     writer.SetInputData(vtp)
-    writer.SetFileName(fln+'_%d.vtp'%k)
+    writer.SetDataModeToBinary()
+    writer.SetCompressorTypeToNone()
+#   writer.SetCompressorTypeToZLib()
+    writer.SetWriteToOutputString(True)
+#   writer.SetFileName(fln+'_%d.vtp'%k)
     writer.Update()
+    return writer.GetOutputString()
 #
-def simu(x,aux):
+def simu(x,n,string):
 #
-    n=aux[0]
-    cols=aux[1]
-    tfms=aux[2]
-    objs=aux[3]
-#
-#   invert the current transform (understood to take it back to ref)
-#
+    tfms=[]
     bnds=[]
-    m = int(n*(n-1)/2)
+    objs=[]
     for i in range(n):
-        if i < n-1:
-            tfms[i].Translate(x[i*7+0], x[i*7+1], x[i*7+2])
-            tfms[i].RotateWXYZ(x[i*7+3], x[i*7+4], x[i*7+5], x[i*7+6])
-        else:
-            tfms[i].RotateWXYZ(x[i*7+0], x[i*7+1], x[i*7+2], x[i*7+3])
-        vtp=tran(objs[i],tfms[i])
-        bnds.append(vtp.GetBounds())
-    c=0
-    for i in range(m):
-        cols[i].Update()
-        c=c+cols[i].GetNumberOfContacts()
-    for i in range(n):
-        if i < n-1:
-            tfms[i].RotateWXYZ(-x[i*7+3], x[i*7+4], x[i*7+5], x[i*7+6])
-            tfms[i].Translate(-x[i*7+0], -x[i*7+1], -x[i*7+2])
-        else:
-            tfms[i].RotateWXYZ(-x[i*7+0], x[i*7+1], x[i*7+2], x[i*7+3])
 #
-    for j in range(n):
-        if j == 0:
-            bds=list(bnds[j][:])
+        red = vtk.vtkXMLPolyDataReader()
+        red.ReadFromInputStringOn()
+        red.SetInputString(string[i])
+        red.Update()
+        obj = red.GetOutput()
+        objs.append(obj)
+#
+        tfm = vtk.vtkTransform()
+        if i < n-1:
+            tfm.Translate(x[i*7+0], x[i*7+1], x[i*7+2])
+            tfm.RotateWXYZ(x[i*7+3], x[i*7+4], x[i*7+5], x[i*7+6])
         else:
-            for i in range(6):
-                if i%2 == 0:
-                    bds[i] = min(bds[i],bnds[j][i])
+            tfm.RotateWXYZ(x[i*7+0], x[i*7+1], x[i*7+2], x[i*7+3])
+        tfm.Update()
+#
+        vtp=tran(obj,tfm)
+        bnds.append(vtp.GetBounds())
+        tfms.append(tfm)
+#
+        if i == 0:
+            bds=list(bnds[i][:])
+        else:
+            for j in range(6):
+                if j%2 == 0:
+                    bds[j] = min(bds[j],bnds[i][j])
                 else:
-                    bds[i] = max(bds[i],bnds[j][i])
+                    bds[j] = max(bds[j],bnds[i][j])
+#
+    c=0
+    for i in range(n):
+        for j in range(i+1,n):
+#
+            coli = vtk.vtkCollisionDetectionFilter()
+            coli.SetCollisionModeToFirstContact()
+            coli.SetInputData(0, objs[i])
+            coli.SetTransform(0, tfms[i])
+            coli.SetInputData(1, objs[j])
+            coli.SetTransform(1, tfms[j])
+            coli.Update()
+            c=c+coli.GetNumberOfContacts()
 #
     f=c*1e6
     f=f+(bds[1]-bds[0])*(bds[3]-bds[2])*(bds[5]-bds[4])# + np.linalg.norm(x[:3])**2.
@@ -95,8 +108,6 @@ if __name__ == "__main__":
 #   read in parts
 #
     objs=[]
-    tfms=[]
-#
     for i in range(n):
 #
         red = vtk.vtkSTLReader()
@@ -112,34 +123,14 @@ if __name__ == "__main__":
 #
         [obj,tfm_0,_] = move(obj,-g,[0.,1.,0.,0.])
 #
-        [_,tfm,_] = move(obj,[0.,0.,0.],[0.,1.,0.,0.])
+        obj=wout(obj,'ref',i)
 #
         objs.append(obj)
-        tfms.append(tfm)
-#
-    cols=[]
-#
-    for i in range(n):
-        for j in range(i+1,n):
-#
-            coli = vtk.vtkCollisionDetectionFilter()
-            coli.SetCollisionModeToFirstContact()
-            coli.SetInputData(0, objs[i])
-            coli.SetTransform(0, tfms[i])
-            coli.SetInputData(1, objs[j])
-            coli.SetTransform(1, tfms[j])
-            coli.Update()
-#
-            cols.append(coli)
-#
-    x0=np.zeros(7*n)
 #
     l=[-1e3]*(7*(n-1)+4)
     u=[1e3]*(7*(n-1)+4)
 #
-    aux=[n,cols,tfms,objs]
-#
-    res=differential_evolution(simu,args=(aux,),callback=conv,bounds=list(zip(l,u)),workers=1,seed=69,polish=True,maxiter=1000, init='sobol')
+    res=differential_evolution(simu,args=(n,objs),callback=conv,bounds=list(zip(l,u)),workers=-1,seed=69,polish=True,maxiter=10000, init='sobol')
     print(res)
     x=res.x
     for i in range(n):
