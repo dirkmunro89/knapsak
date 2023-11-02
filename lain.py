58767038bc8d8e216b826a3a3812ae476269aae4
Dirk Munro <dirkmunro8@gmail.com>
2023-11-02 14:33:25 +0100

    rough

--- lain.py
+++ lain.py
@@ -18,7 +18,7 @@ def conv2(xk,args):
     c_a=args[3]
     c_v=args[4]
     [f,c]=simu(xk,n,objs,c_l,c_a,c_v,1)
-    print('%14.3e %6d'%(f,c))
+    print('%14.3e %6d'%(f,c),flush=True)
 #
 #   for i in range(n):
 #
@@ -46,7 +46,7 @@ def conv(xk,convergence,args):
     c_a=args[3]
     c_v=args[4]
     [f,c]=simu(xk,n,objs,c_l,c_a,c_v,1)
-    print('%7.3f %14.3e %6d'%(convergence,f,c))
+    print('%7.3f %14.3e %6d'%(convergence,f,c),flush=True)
 #
     for i in range(n):
 #
@@ -56,11 +56,11 @@ def conv(xk,convergence,args):
         red.Update()
         obj = red.GetOutput()
 #
-        if i < n-1:
-            tmp = xk[7*i:7*i+7]
-        else:
-            tmp=np.zeros(7)
-            tmp[3:]=xk[7*i:7*i+4]
+#       if i < n-1:
+        tmp = xk[7*i:7*i+7]
+#       else:
+#           tmp=np.zeros(7)
+#           tmp[3:]=xk[7*i:7*i+4]
         [vtp,_,_] = move(obj,tmp[:3],tmp[3:7],c_l,c_a)
         woutfle(vtp,'see',i)
 #
@@ -126,11 +126,11 @@ def simu(x,n,string,c_l,c_a,c_v,flg):
         objs.append(obj)
 #
         tfm = vtk.vtkTransform()
-        if i < n-1:
-            tfm.Translate(c_l*x[i*7+0], c_l*x[i*7+1], c_l*x[i*7+2])
-            tfm.RotateWXYZ(c_a*x[i*7+3], x[i*7+4], x[i*7+5], x[i*7+6])
-        else:
-            tfm.RotateWXYZ(c_a*x[i*7+0], x[i*7+1], x[i*7+2], x[i*7+3])
+#       if i < n-1:
+        tfm.Translate(c_l*x[i*7+0], c_l*x[i*7+1], c_l*x[i*7+2])
+        tfm.RotateWXYZ(c_a*x[i*7+3], x[i*7+4], x[i*7+5], x[i*7+6])
+#       else:
+#           tfm.RotateWXYZ(c_a*x[i*7+0], x[i*7+1], x[i*7+2], x[i*7+3])
         tfm.Update()
 #
         vtp=tran(obj,tfm)
@@ -161,7 +161,7 @@ def simu(x,n,string,c_l,c_a,c_v,flg):
             coli.Update()
             c=c+coli.GetNumberOfContacts()
 #
-    f=(bds[5]-bds[4])# + np.linalg.norm(x[:3])**2.
+    f=bds[5]#-bds[4])# + np.linalg.norm(x[:3])**2.
 #   f=(bds[1]-bds[0])*(bds[3]-bds[2])*(bds[5]-bds[4])# + np.linalg.norm(x[:3])**2.
 #
 #   f=f/c_v 
@@ -181,6 +181,9 @@ def simu(x,n,string,c_l,c_a,c_v,flg):
         y=y+1
     if bds[3]>ext:
         b=b+(abs(bds[3])-ext)**1.
+        y=y+1
+    if bds[4]<0.:
+        b=b+(abs(bds[4]))**1.
         y=y+1
 #
     f=f+b*c_l
@@ -239,14 +242,14 @@ if __name__ == "__main__":
 #
     c_l=c_l*n*2
 #
-    l=[-1e0]*(7*(n-1)+4)
-    u=[1e0]*(7*(n-1)+4)
+    l=[-1e0]*(7*n)
+    u=[1e0]*(7*n)
 #
-    res=differential_evolution(simu,args=(n,objs,c_l,c_a,c_v,0),callback=partial(conv,args=(n,objs,c_l,c_a,c_v)),bounds=list(zip(l,u)),workers=4,seed=1,polish=False,disp=True,maxiter=10,updating='deferred')
+    res=differential_evolution(simu,args=(n,objs,c_l,c_a,c_v,0),callback=partial(conv,args=(n,objs,c_l,c_a,c_v)),bounds=list(zip(l,u)),workers=4,seed=1,polish=False,disp=True,maxiter=1000,updating='deferred')
 #
     x0=res.x
-    bds=[[-1.,1.] for i in range(7*(n-1)+4)]; tup_bds=tuple(bds)
-    print(res)
+    bds=[[-1.,1.] for i in range(7*n)]; tup_bds=tuple(bds)
+    print(res,flush=True)
     x=res.x
     for i in range(n):
 #
@@ -256,11 +259,11 @@ if __name__ == "__main__":
         red.Update()
         obj = red.GetOutput()
 #
-        if i < n-1:
-            tmp = x[7*i:7*i+7]
-        else:
-            tmp=np.zeros(7)
-            tmp[3:]=x[7*i:7*i+4]
+#       if i < n-1:
+        tmp = x[7*i:7*i+7]
+#       else:
+#           tmp=np.zeros(7)
+#           tmp[3:]=x[7*i:7*i+4]
         [vtp,_,_] = move(obj,tmp[:3],tmp[3:7],c_l,c_a)
         woutfle(vtp,'see',i)
 #
@@ -270,10 +273,10 @@ if __name__ == "__main__":
     while abs(f-fold)>0.1:
         fold=f
         res=minimize(simu,args=(n,objs,c_l,c_a,c_v,0), x0=x0, bounds=tup_bds, method='Nelder-Mead', options={'disp': True, 'adaptive': True},callback=partial(conv2,args=(n,objs,c_l,c_a,c_v)))#, 'eps': 1e-32, 'gtol': 1e-32, 'tol': 1e-32, 'xatol': 1e-16, 'fatol': 1e-16})
-        print(res)
+        print(res,flush=True)
         x0=res.x
         f=res.fun
-        print(f,x)
+        print(f,x,flush=True)
 #   res=minimize(simu,args=(n,objs,c_l,c_a,c_v,0), x0=x0, bounds=tup_bds, method='Nelder-Mead', options={'disp': True,'maxfev': 1000000, 'maxiter': 1000000})#, 'eps': 1e-32, 'gtol': 1e-32, 'tol': 1e-32, 'xatol': 1e-16, 'fatol': 1e-16})
 #
 #   print(res)
@@ -292,11 +295,11 @@ if __name__ == "__main__":
             red.Update()
             obj = red.GetOutput()
 #
-            if i < n-1:
-                tmp = x0[7*i:7*i+7]
-            else:
-                tmp=np.zeros(7)
-                tmp[3:]=x0[7*i:7*i+4]
+#           if i < n-1:
+            tmp = x0[7*i:7*i+7]
+#           else:
+#               tmp=np.zeros(7)
+#               tmp[3:]=x0[7*i:7*i+4]
             [vtp,_,_] = move(obj,tmp[:3],tmp[3:7],c_l,c_a)
             woutfle(vtp,'lee',i)
     stop
