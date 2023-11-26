bcbf33266201608674cb8a992ba5d76a23e2b790
Dirk Munro <dirkmunro8@gmail.com>
2023-11-26 10:58:26 +0100

    trying windows makefile

--- setup.py
+++ setup.py
@@ -1,22 +1,13 @@
-from setuptools import setup, find_packages
-
-_py3_packages = find_packages(include=['./'])
+from setuptools import setup
 
 setup(
-    name="Python 3",
-    version="0.0.1",
-    description='General environment for Python 3',
+    name="knapsak",
     author_email='dirkmunro8@gmail.com',
-    packages=_py3_packages,
-    keywords=['Python 3'],
+    packages=[],
     python_requires='>=3.8, <4',
     install_requires=[
         'numpy',
         'scipy',
-        'matplotlib',
-        'cvxopt',
-        'pytest',
-        'joblib',
         'vtk'
     ]
 )
